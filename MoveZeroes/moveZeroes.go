632b22ccc007a2dbf99c39a3300aa313cb10671a
guyikang <570385841@qq.com>
2018-12-29 11:45:47 +0800

    update

---
+++ MoveZeroes/moveZeroes.go
@@ -0,0 +1,45 @@
+package moveZeroes
+
+// 移动零
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+// 示例:
+// 输入: [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+
+// 说明:
+// 必须在原数组上操作，不能拷贝额外的数组。
+// 尽量减少操作次数。
+
+func MoveZeroes(nums []int) {
+	moveZeroes(nums)
+}
+
+// func moveZeroes(nums []int) {
+// 	length := len(nums)
+// 	j := 0
+// 	for i := 0; i < length; i++ {
+// 		if nums[i] != 0 {
+// 			nums[j] = nums[i]
+// 			j++
+// 		}
+// 	}
+
+// 	for i := j; i < length; i++ {
+// 		nums[i] = 0
+// 	}
+// }
+
+func moveZeroes(nums []int) {
+	for index, num := range nums {
+		if num == 0 {
+			for i := index; i < len(nums); i++ {
+				if nums[i] != 0 {
+					nums[index] = nums[i]
+					nums[i] = 0
+					break
+				}
+			}
+		}
+	}
+}
